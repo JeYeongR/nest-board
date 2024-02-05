ede9922f78ac8750e011bde69e5f73c9dddbd29e
JeYeongR <wpdud2003@gmail.com>
2024-02-05 23:16:34 +0900

    Test: 유저 생성 API 단위 테스트 추가

---
+++ src/user/user.controller.spec.ts
@@ -0,0 +1,54 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+
+describe('UserController', () => {
+  let userController: UserController;
+
+  const mockUserService = {
+    createUser: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UserController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: mockUserService,
+        },
+      ],
+    }).compile();
+
+    userController = module.get<UserController>(UserController);
+  });
+
+  it('should be defined', () => {
+    expect(userController).toBeDefined();
+  });
+
+  describe('createUser()', () => {
+    const createDto = {
+      email: 'test@email',
+      password: 'test1234',
+    };
+
+    it('SUCCESS: 유저 서비스를 정상적으로 호출한다.', async () => {
+      // Given
+      const spyCreateUserFn = jest.spyOn(mockUserService, 'createUser');
+
+      const expectedResult = {
+        message: 'CREATE_SUCCESS',
+        statusCode: 201,
+      };
+
+      // When
+      const result = await userController.createUser(createDto);
+
+      // Then
+      expect(result).toEqual(expectedResult);
+      expect(spyCreateUserFn).toHaveBeenCalledTimes(1);
+      expect(spyCreateUserFn).toHaveBeenCalledWith(createDto);
+    });
+  });
+});
