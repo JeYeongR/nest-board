36b8ecc79749f502a28ba32385075c09b059b203
JeYeongR <wpdud2003@gmail.com>
2024-02-06 02:54:34 +0900

    Chore: jwtConfig 사용 안 하는 옵션 제거

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -4,7 +4,6 @@ import { JwtModuleAsyncOptions } from '@nestjs/jwt';
 export const jwtConfig: JwtModuleAsyncOptions = {
   imports: [ConfigModule],
   useFactory: async (configService: ConfigService) => ({
-    global: true,
     secret: configService.get('JWT_SECRET_KEY'),
   }),
   inject: [ConfigService],
