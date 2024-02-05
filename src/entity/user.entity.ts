0991e51d97d0cf6efad8feffa7ad7a47cbfca1c5
JeYeongR <wpdud2003@gmail.com>
2024-02-05 18:47:49 +0900

    Feat: 엔티티 수정

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -5,7 +5,10 @@ export class User {
   @PrimaryGeneratedColumn({ type: 'bigint' })
   id: number;
 
-  @Column({ length: 30 })
+  @Column({
+    length: 30,
+    unique: true,
+  })
   email: string;
 
   @Column({ length: 20 })
