6a35812cf3c383c50d1f9a2d3251bb0a06435338
JeYeongR <wpdud2003@gmail.com>
2024-02-06 15:05:47 +0900

    Feat: 엑세스토큰 재발급 API 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { GetUser } from '../common/decorator/get-user.decorator';
 import { CommonResponseDto } from '../common/dto/common-response.dto';
 import { ResponseMessage } from '../common/dto/response-message.enum';
+import { RefreshTokenAuthGuard } from '../common/guard/refresh-token-auth.guard';
+import { User } from '../entity/user.entity';
 import { AuthService } from './auth.service';
 import { DoLoginDto } from './dto/do-login.dto';
 import { TokenDto } from './dto/token.dto';
@@ -18,6 +28,19 @@ export class AuthController {
 
     return CommonResponseDto.success<TokenDto>(
       ResponseMessage.LOGIN_SUCCESS,
+    ).setData(result);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('/reissue')
+  @UseGuards(RefreshTokenAuthGuard)
+  async reissueAccessToken(
+    @GetUser() user: User,
+  ): Promise<CommonResponseDto<TokenDto>> {
+    const result = await this.authService.reissueAccessToken(user);
+
+    return CommonResponseDto.success<TokenDto>(
+      ResponseMessage.REISSUE_SUCCESS,
     ).setData(result);
   }
 }
