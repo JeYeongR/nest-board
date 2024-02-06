710f466b270a464493fc89a550f1b864047d7225
JeYeongR <wpdud2003@gmail.com>
2024-02-06 13:50:11 +0900

    Refactor: Auth 가드에 리프레시 토큰 로직 추가

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -6,6 +6,7 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import { User } from '../entity/user.entity';
 import { UserService } from '../user/user.service';
 
 @Injectable()
@@ -17,24 +18,40 @@ export class AuthGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    const token = this.extractTokenFromHeader(request);
-    if (!token) {
-      throw new UnauthorizedException();
-    }
+    const accessToken = this.extractAccessTokenFromHeader(request);
+    const refreshToken = this.extractRefreshTokenFromHeader(request);
+
     try {
-      const { sub: id } = await this.jwtService.verifyAsync(token);
-      const foundUser = await this.userService.findOneById(id);
+      if (!accessToken) throw new UnauthorizedException();
+
+      let foundUser: User;
+      if (accessToken && refreshToken) {
+        foundUser = await this.getUserFromToken(refreshToken);
+      } else {
+        foundUser = await this.getUserFromToken(accessToken);
+      }
 
       request.user = foundUser;
+      return true;
     } catch (error) {
       throw new UnauthorizedException();
     }
-
-    return true;
   }
 
-  private extractTokenFromHeader(request: Request): string | undefined {
+  private extractAccessTokenFromHeader(request: Request): string | undefined {
     const [type, token] = request.headers.authorization?.split(' ') ?? [];
     return type === 'Bearer' ? token : undefined;
+  }
+
+  private extractRefreshTokenFromHeader(request: Request): string | undefined {
+    const refreshToken = request.headers.refreshtoken as string;
+    const [type, token] = refreshToken?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+
+  async getUserFromToken(token: string) {
+    const { sub: id } = await this.jwtService.verifyAsync(token);
+    const foundUser = await this.userService.findOneById(id);
+    return foundUser;
   }
 }
