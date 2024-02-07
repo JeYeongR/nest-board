1862ca3944b1a418929964e637ed98a18d1ca15f
JeYeongR <wpdud2003@gmail.com>
2024-02-07 19:44:17 +0900

    Feat: PostCategory Enum 수정

--- src/post/dto/post-category.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum PostCategory {
-  NOTICE = '공지사항',
-  QUESTION_AND_ANSWER = 'Q&A',
-  ASK = '1:1문의',
-}
---
+++ src/post/enum/post-category.enum.ts
@@ -0,0 +1,5 @@
+export enum PostCategory {
+  NOTICE = 'notice',
+  QUESTION_AND_ANSWER = 'qa',
+  ASK = 'ask',
+}
