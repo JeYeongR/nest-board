6bde69787c8c6bd68d15e433c27fb1b5719129db
JeYeongR <wpdud2003@gmail.com>
2024-02-09 00:54:16 +0900

    Test: 글 조회 API 단위 테스트 추가

--- src/post/post.controller.spec.ts
+++ src/post/post.controller.spec.ts
@@ -9,6 +9,7 @@ describe('PostController', () => {
 
   const mockPostService = {
     createPost: jest.fn(),
+    getPosts: jest.fn(),
   };
 
   beforeEach(async () => {
@@ -72,6 +73,65 @@ describe('PostController', () => {
         images as Express.MulterS3.File[],
         createPostDto,
       );
+    });
+  });
+
+  describe('getPosts()', () => {
+    const getPostDto = {
+      pageNo: 1,
+      category: PostCategory.NOTICE,
+      getOffset: function (): number {
+        return 0;
+      },
+      getLimit: function (): number {
+        return 10;
+      },
+    };
+    const user = {
+      id: 1,
+      email: 'test@email',
+      nickname: 'test',
+      password: '$2b$10$Tuip8DXQlXtBaTVJvpvZ0eIfrxkXktGTSF4ew4HSdvWD7MRF.gykO',
+    };
+    const mockPostResponseDtos = [
+      {
+        id: 1,
+        title: '치킨',
+        viewCount: 1,
+        createdAt: new Date(),
+        isMyPost: true,
+        userDto: {
+          id: 1,
+          nickname: '사과',
+        },
+      },
+    ];
+    const mockPageResponseDto = {
+      currentPage: getPostDto.pageNo,
+      pageSize: getPostDto.getLimit(),
+      totalCount: mockPostResponseDtos.length,
+      totalPage: Math.ceil(mockPostResponseDtos.length / getPostDto.getLimit()),
+      items: mockPostResponseDtos,
+    };
+
+    it('SUCCESS: 포스트 서비스를 정상적으로 호출한다.', async () => {
+      // Given
+      const spyGetPostsFn = jest.spyOn(mockPostService, 'getPosts');
+      spyGetPostsFn.mockResolvedValueOnce(mockPageResponseDto);
+
+      const expectedResult = {
+        message: 'READ_SUCCESS',
+        statusCode: 200,
+        data: mockPageResponseDto,
+      };
+
+      // When
+      const result = await postController.getPosts(getPostDto, user);
+
+      // Then
+      expect(result).toEqual(expectedResult);
+      expect(spyGetPostsFn).toHaveBeenCalledTimes(1);
+      expect(spyGetPostsFn).toHaveBeenCalledWith(getPostDto, user.id);
     });
   });
 });
