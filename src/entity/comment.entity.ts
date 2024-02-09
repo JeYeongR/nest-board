ef153859e0916535ef831eee35ae05320510f106
JeYeongR <wpdud2003@gmail.com>
2024-02-09 20:47:44 +0900

    Feat: 댓글 엔티티 연관관계 추가

--- src/entity/comment.entity.ts
+++ src/entity/comment.entity.ts
@@ -1,11 +1,15 @@
 import {
   Column,
   Entity,
+  JoinColumn,
+  ManyToOne,
   PrimaryGeneratedColumn,
   Tree,
   TreeChildren,
   TreeParent,
 } from 'typeorm';
+import { Post } from './post.entity';
+import { User } from './user.entity';
 
 @Entity()
 @Tree('closure-table')
@@ -21,4 +25,12 @@ export class Comment {
 
   @TreeParent()
   parent: Comment;
+
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @ManyToOne(() => Post, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'post_id' })
+  post: Post;
 }
